c8ce21cb1d6fbd1e2525bcd7fd69e48e30d27bd4
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-07 18:32:30 -0500

    Reformat table_ids.py for consistency

--- BootsForTest/table_ids.py
+++ BootsForTest/table_ids.py
@@ -2,10 +2,11 @@ import pandas as pd
 dicc = {'IDs': [],'email': []} 
 for i in range(2000,4030):
     dicc['IDs'].append(f'PR00{i}')
-    dicc['emai'].append(f'alumn{i}@gmail.com')
+    dicc['email'].append(f'alumn{i}@gmail.com')
 
 df = pd.DataFrame(dicc)
 df.to_csv('dicc.csv', index= False)
+
 
 
 
