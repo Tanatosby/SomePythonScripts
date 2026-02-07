95ad29aa680087ed69cb85c108acb3128232845a
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-06 21:31:45 -0500

    Update atabla.py

---
+++ atabla.py
@@ -0,0 +1,19 @@
+s the string that represents the title of the column to extract. 
+  for l in f:
+    lr = l.strip()
+    if lr.startswith(t): 
+      ll = lr.split()
+      dicce[t] = [ll[1]]
+      for i in range(len(2:ll)):
+        dicce[t].append(i)
+
+t1 = 'column 1'
+t2 = 'column 2'
+t3 = 'column 2'
+
+column(f,t1)
+column(f,t2)
+column(f,t3)
+
+df = pd.DataFrame(ficce)
+df.to_excel('file.xlsx')
