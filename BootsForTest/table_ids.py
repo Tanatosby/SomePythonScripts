89760aa9f2339d2224b83d1177bed4ff7bc4cc5b
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-02-07 18:31:24 -0500

    Refactor ID generation and add email field

--- BootsForTest/table_ids.py
+++ BootsForTest/table_ids.py
@@ -1,15 +1,11 @@
 import pandas as pd
-dicc = {'IDs': []} 
-dicc2 = {'IDs': []}
-for i in range(2000,3001):
+dicc = {'IDs': [],'email': []} 
+for i in range(2000,4030):
     dicc['IDs'].append(f'PR00{i}')
+    dicc['emai'].append(f'alumn{i}@gmail.com')
 
-for i in range(3002,4030):
-    dicc2['IDs'].append(f'PR00{i}')
 df = pd.DataFrame(dicc)
-df.to_csv('dicc1.csv', index= False)
-df = pd.DataFrame(dicc2)
-df.to_csv('dicc2.csv', index= False)
+df.to_csv('dicc.csv', index= False)
 
 
 
